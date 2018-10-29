8a9ec13db00f9123b99694a972d7d9fdd012259a
elearic <elearic@aliyun.com>
2018-10-30 00:48:40 +0800

    dav 版本升级2^

---
+++ src/components/User/UserPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainLayout from '../../MainLayout/MainLayout';
+
+const UserPage = (props) => {
+  
+  return (
+    <MainLayout>
+        <div>Hello world</div>
+    </MainLayout>
+    );
+};
+
+UserPage.propTypes = {
+};
+
+export default UserPage;
--- src/router.js
+++ src/router.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
 import MainLayout from './MainLayout/MainLayout';
+import UserPage from './components/User/UserPage';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
+     <div>
       <Route path="/" component={MainLayout} />
+      <Route path="/page01" component={UserPage} />
+      </div>
     </Router>
   );
 }
